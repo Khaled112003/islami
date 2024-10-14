9cd47afdd93cf2d69b63b84a0bc8e1680e437094
Khaled Mohaseb <khaledmohaseb319@gmail.com>
2024-10-14 23:39:31 +0300

    create fun signup & save date for user & handle with errors from firebase

---
+++ lib/Features/authentication/data/repo/auth_repo.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:islami/Features/authentication/data/model/user_model.dart';
+import 'package:islami/core/errors/failure.dart';
+import 'package:islami/core/errors/firebase_auth_errors.dart';
+import 'package:islami/core/errors/firestor.dart';
+
+class AuthRepo {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+
+  Future<Either<Failure, UserModel>> signUpWithEmailAndPass(
+      String email, String userName, String password) async {
+    try {
+      final credential = await _firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      final user = credential.user;
+      
+      if (user != null) {
+        final newUser = UserModel(uid: user.uid, username: userName, email: email);
+        await saveData(newUser);
+        return Right(newUser);  
+      } else {
+        return Left(FirebaseAuthFailure('User is null'));
+      }
+    } catch (error) {
+      if (error is FirebaseAuthException) {
+        return Left(FirebaseAuthFailure.fromFirebaseAuthException(error));
+      } else if (error is FirebaseException) {
+        return Left(CloudFirestoreFailure.fromFirebaseCoreException(error));
+      } else {
+        return Left(FirebaseAuthFailure(error.toString()));
+      }
+    }
+  }
+
+  Future<void> saveData(UserModel user) async {
+    final userCollection = _firebaseFirestore.collection('user');
+    await userCollection.doc(user.uid).set(user.toMap());
+  }
+}
+
