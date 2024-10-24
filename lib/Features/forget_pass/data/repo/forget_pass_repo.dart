0ea636bc6f1de0424843c9a6e4e704e18b1f6513
Khaled Mohaseb <khaledmohaseb319@gmail.com>
2024-10-24 14:25:54 +0300

    update repo

--- lib/Features/forget_pass/data/repo/forget_pass_repo.dart
+++ lib/Features/forget_pass/data/repo/forget_pass_repo.dart
@@ -2,13 +2,20 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class ForgetPassRepo {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-  Future<void> signUpWithEmailAndPass(String email) async {
-    try {
-       await _firebaseAuth.sendPasswordResetEmail(
-        email: email,
-      );
-    } catch (error) {
-      throw Exception('Failed to send reset email: $error');
+
+  Future<void> signUpWithEmailAndPass() async {
+    User? user = _firebaseAuth.currentUser; 
+    
+    if (user != null && !user.emailVerified) {
+      try {
+        await user.sendEmailVerification();
+        print('تم إرسال رابط التحقق إلى ${user.email}');
+      } catch (error) {
+        print('فشل إرسال رابط التحقق: $error');
+        throw('فشل إرسال رابط التحقق: $error');
+      }
+    } else {
+      print('المستخدم غير موجود أو تم التحقق من البريد الإلكتروني بالفعل');
     }
   }
 }
